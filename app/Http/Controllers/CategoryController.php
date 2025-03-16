1712dfab443a872d0a3f7234b6333167e1c6387f
Jimmy Vazquez <jv78676@gmail.com>
2025-03-16 12:21:38 -0600

    Se agregaron los controladores y modelos tambien se hizo la instalacion de API para su uso y registro de las rutas en api.php

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //  Retorna todas las categorías
+        return Category::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Crea una nueva categoría
+        $request->validate([
+            'name'=> 'required|string'
+        ]);
+
+        $category = Category::create($request->all());
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
